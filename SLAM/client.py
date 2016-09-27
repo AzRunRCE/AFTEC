193ca6e1ad99b3375245d0a62e66436f2b5c920c
root <root@localhost.localdomain>
2016-09-27 17:22:09 +0200

    update

--- SLAM/client.py
+++ SLAM/client.py
@@ -5,13 +5,13 @@ import os
 import socket
 hote = "localhost"
 port = 15555
+user = input("Username:") + ": "
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_serveur.connect((hote, port))
 print("Connexion établie avec le serveur sur le port {}".format(port))
-all_msg = ""
 msg_a_envoyer = b""
 while msg_a_envoyer != b"fin":
-    msg_a_envoyer = input(">")
+    msg_a_envoyer = user + input(">")
     msg_a_envoyer = msg_a_envoyer.encode()
     connexion_avec_serveur.send(msg_a_envoyer)
 print("Fermeture de la connexion")
